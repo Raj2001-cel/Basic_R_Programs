c78fe3bdd407ac17190b60df546edf67bcf3e03f
Raj Mourya <rajhmourya@gmail.com>
2022-11-02 20:46:38 +0530

    master | data types in R

---
+++ datatypes.R
@@ -0,0 +1,20 @@
+num1 <- 12.3
+print(class(num1)) #numeric
+
+
+num  <- 12
+print(class(num)) # numeric
+
+
+int <- 123L
+print(typeof(int)) # integer
+
+
+comp <- 3+2i
+print(class(comp)) # complex
+
+charac <- "hello"
+print(class(charac)) # character
+
+
+print(class(charToRaw(charac))) #raw
