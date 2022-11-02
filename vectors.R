e6cf7b4941deedbbd29097b9c13eb21036e36dc3
Raj Mourya <rajhmourya@gmail.com>
2022-11-02 22:11:25 +0530

    added list

--- vectors.R
+++ vectors.R
@@ -8,7 +8,15 @@ print(v2)
 
 print(v1[2]) #2
 print(v1[[2]]) #2
-
 print(v2[c(1,2,3)]) # 5 6 7
+
+
+#lists in R
+
+list_data <- list("okay", "fine", c(1,2,1), FALSE, 1.23)
+print(class(list_data[[1]]))
+
+
+
 
 
