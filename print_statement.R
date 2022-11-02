d2e9f0fa568f6377e5191feb2aa25d3029e65be6
Raj Mourya <rajhmourya@gmail.com>
2022-11-02 20:39:12 +0530

    master | print statements

---
+++ print_statement.R
@@ -0,0 +1,20 @@
+# the function used to print statements is print()
+print("raj mourya")
+
+# lets print a character variable 
+mystring <- "hello world"
+print(mystring)
+
+
+#paste function
+print(paste("college","Engineering",sep = "-"))
+
+#paste0 function
+print(paste0("hello","- yash"))
+
+print(3+4) # 7
+
+print(7==7) #TRUE
+
+
+
