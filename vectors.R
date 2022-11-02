f29681b217fa1297c708bb7e763339b424076a43
Raj Mourya <rajhmourya@gmail.com>
2022-11-02 22:19:19 +0530

    master | matrix

--- vectors.R
+++ vectors.R
@@ -14,8 +14,19 @@ print(v2[c(1,2,3)]) # 5 6 7
 #lists in R
 
 list_data <- list("okay", "fine", c(1,2,1), FALSE, 1.23)
-print(class(list_data[[1]]))
+print(class(list_data[[1]])) #character
+print(class(list_data[[3]])) #numeric
 
+
+# matrices in R
+
+M <- matrix(c(1,2,3,4,5,6),nrow = 2)
+print(M)
+#[,1] [,2] [,3]
+#[1,]    1    3    5
+#[2,]    2    4    6
+
+print(M[1,2])  #3
 
 
 
